0ec2c32dd3f6596549e19117d6a7f1653e0505d3
jonnSmith <eugenpushkaroff@gmail.com>
2020-12-18 20:31:53 +0200

    Fix for unmpunt forms

--- client/src/ui/templates/user/sign-in/index.tsx
+++ client/src/ui/templates/user/sign-in/index.tsx
@@ -25,8 +25,13 @@ const UserSignIn = (props: ISignInProps) => {
     Object.keys(SignInFormInitialObject).forEach( (k) => {
       SignInFormInitialObject[k as keyof ISignInForm] = (el.namedItem(k) as HTMLInputElement).value;
     });
+    // signIn({variables: SignInFormInitialObject}).then(({data}) => {
+    //   dispatch({type: ACTIONS.HANDLE_PAYLOAD, payload: data?.payload});
+    //   disable();
+    // });
     onSubmit(SignInFormInitialObject).then(() => {
-      disable();
+      // TODO: React state update on an unmounted component.
+      // disable();
     });
   };
 
